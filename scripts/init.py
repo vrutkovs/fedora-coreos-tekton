79f4e8140969b238663ba6f116b1b33879f924ef
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-09-11 17:31:52 +0200

    scripts/init: use check_output

--- scripts/init.py
+++ scripts/init.py
@@ -4,4 +4,4 @@
 import subprocess
 
 #TODO: make ref and src config urls configurable
-subprocess.check_call(["coreos-assembler", "init", "--force", "--branch", "master", "https://github.com/coreos/fedora-coreos-config"])
+subprocess.check_output(["coreos-assembler", "init", "--force", "--branch", "master", "https://github.com/coreos/fedora-coreos-config"])
